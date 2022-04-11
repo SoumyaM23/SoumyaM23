2211fb504c2f5d6f190659a60ac080f0b54741d3
SoumyaM23 <88240689+SoumyaM23@users.noreply.github.com>
2022-04-11 10:13:12 +0530

    Create abstract_cls.cs

---
+++ abstract_cls.cs
@@ -0,0 +1,23 @@
+using System;
+
+public abstract class A
+{
+	public abstract void show();
+}
+
+public class B : A
+{
+ public override void show()
+ {
+	 Console.WriteLine("Overriding show");
+ }
+}
+
+public class Test{
+	public static void Main(String[] args)
+	{
+	 B b = new B();
+	 b.show();
+	 	
+	}
+}
